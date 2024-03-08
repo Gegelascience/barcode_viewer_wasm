939550da3050a8ce1cc549af25b78176f742d3d8
VERCHERE Rémi <remi.verchere2@gmail.com>
2024-03-08 21:43:20 +0100

    calcul code barre

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use wasm_bindgen::prelude::*;
 mod ean_checker;
+mod barcode_data;
+
+use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
 
 #[wasm_bindgen]
 extern {
@@ -13,5 +16,17 @@ pub fn greet(name: &str) {
 
 #[wasm_bindgen]
 pub fn check_ean(possible_ean: &str) -> bool {
-    return ean_checker::is_correct_ean(possible_ean);
+    ean_checker::is_correct_ean(possible_ean)
 }
+
+#[wasm_bindgen]
+pub fn get_ean_13_barcode_data(possible_ean: &str) -> String {
+    if ean_checker::is_correct_ean(possible_ean) {
+        return barcode_data::calculate_barcode_ean13(possible_ean);
+    }
+    String::from("")
+    
+}
+
+
+// webgl
