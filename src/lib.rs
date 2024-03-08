a0078b9957b7e71dc07002ce60afab880058f2df
VERCHERE Rémi <remi.verchere2@gmail.com>
2024-03-08 23:31:30 +0100

    minimal test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use wasm_bindgen::prelude::*;
 mod ean_checker;
 mod barcode_data;
+use wasm_bindgen::JsCast;
 
-use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
+extern crate js_sys;
 
 #[wasm_bindgen]
 extern {
@@ -30,3 +31,27 @@ pub fn get_ean_13_barcode_data(possible_ean: &str) -> String {
 
 
 // webgl
+#[wasm_bindgen]
+pub fn draw_barcode(barcode_data: &str) -> Result<String, JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("barcodeCanvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let ctx = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+
+    for index in 0..barcode_data.len() {
+        //draw_line(ctx, [(25 + 5*index) as f64, 100.0], [(25 + 5*index) as f64, 350.0], Some("black"), Some(5.0));
+        if barcode_data.chars().nth(index.try_into().unwrap()).unwrap() == '1' {
+            ctx.set_stroke_style(&JsValue::from_str("black"));
+            ctx.set_line_width(5.0);
+            ctx.begin_path();
+            ctx.move_to((25 + 5*index) as f64, 100.0);
+            ctx.line_to((25 + 5*index) as f64, 350.0);
+            ctx.stroke();
+        }
+        
+    }
+    
+    Ok( String::from(barcode_data))
+
+}
